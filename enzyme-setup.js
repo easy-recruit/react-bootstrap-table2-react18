a16c68c2b5bf4119af3da836791cba75e00ca4e4
Dave Carlson <me@davecarlson.co.uk>
2023-07-11 07:11:04 +0100

    react 18 support - few failing tests

--- enzyme-setup.js
+++ enzyme-setup.js
@@ -1,8 +1,21 @@
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import { configure } from 'enzyme';
 
 const configureEnzyme = () => {
   configure({ adapter: new Adapter() });
 };
+
+const consoleError = console.error;
+let mockConsoleError = jest.spyOn(console, 'error').mockImplementation((...args) => {
+    const message = typeof args[0] === 'string' ? args[0] : '';
+    if (
+      message.includes('When testing, code that causes React state updates should be wrapped into act(...)') ||
+      message.includes('antd')
+    ) {
+      return;
+    }
+
+    return consoleError.call(console, args);
+  });
 
 configureEnzyme();
